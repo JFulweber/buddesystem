d54d2cf9798e8bda366863c0e62a3be4d71d6d58
Adair Fulweber <adair625@gmail.com>
2017-10-30 13:09:15 -0500

    mutation addition

--- src/server/graphql/user.js
+++ src/server/graphql/user.js
@@ -1,13 +1,8 @@
 var typeDefs = `
 
-    scalar Date {
-        day: Int,
-        month: Int,
-        year: Int
-    }
-
     type Query {
         users: [User]!
+        user(username:String!): User
     }
 
     type User {
@@ -18,6 +13,17 @@ var typeDefs = `
         interests: [String],
         friends: [User]
     }
+
+    input UserInput {
+        username: String!,
+        email: String!
+    }
+
+    type Mutation {
+        addUser(new_user:UserInput!): Boolean!
+    }
+
+
 `
 
 var resolvers = {
@@ -34,12 +40,37 @@ var resolvers = {
                     });
                 });
             });  
+        },
+        user: async function (parent, args, {User}){
+            var mongoose = require('mongoose');
+            return await new Promise((resolve, reject)=>{
+                mongoose.connect('localhost:27017', function(err){
+                    if(err) reject(err);
+                    User.findOne({username:args.username}).then(result=>{
+                        resolve(result);
+                        mongoose.disconnect();
+                    })
+                });
+            })
+        }
+    },
+    Mutation: {
+        addUser: async function (parent, args, {User}){
+            var mongoose = require('mongoose');
+            return await new Promise((resolve,reject)=>{
+                mongoose.connect('localhost:27017', function(err){
+                    if(err) reject(err);
+                    args.new_user.joined = Date.now();
+                    var myUser = new User(args.new_user);
+                    myUser.date = Date.now();
+                    myUser.save().then(()=>{
+                        resolve(true);
+                        mongoose.disconnect();
+                    });
+                });
+            })
         }
     }
-}
-
-const getUsers = function(User){
-    
 }
 
 export default { typeDefs, resolvers };

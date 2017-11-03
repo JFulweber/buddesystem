4572a8c50449cfca224a11adc8e5a622b0edc95a
Fulweber <FULWEJAM000@mysbisd.org>
2017-11-03 14:05:13 -0500

    web client work

--- src/app/pages/HomePage.jsx
+++ src/app/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import styles from './homepage.scss';
 
 export default class HomePage extends React.Component{
     render(){
+        console.log(styles);
         return(
             <div id = 'homepage' className = {styles.homepage}>
                 <p> Welcome to the BuddE System </p>
